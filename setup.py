3ccdfb64c6c28a0b64ebc9e6bad1cc894e1ba2e2
patschwork <inetreg@pat-schmitz.de>
2023-07-03 20:59:47 +0200

    changed

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
    name='py_db_logger',
-   version='1.1',
-   description='A log handler to write to different type of databases with a file as fallback solution',
+   version='1.1',  # enhanced from forked version
+   description='A log handler to write to different type of databases with a file writer as fallback solution',
    author='Reza AGHaMohammadi / enhanced by Patrick Schmitz',
    author_email='foomail@foo.example',
    packages=['db_logger'],
